70551ffa843059906432f9a420e7ccfbedf7f163
Jeongseok Lee <jslee02@gmail.com>
2015-01-14 18:07:57 -0500

    Try to install gcc-4.8

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -7,6 +7,14 @@ before_install() {
   (cd nlopt-2.4.1/; sh autogen.sh; make CPPFLAGS='-fPIC' && sudo make install)
 }
 
+# Install gcc 4.8 for C++11
+sudo apt-get --yes install python-software-properties 
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get --yes install gcc-4.8
+sudo apt-get --yes autoremove
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+
 # Install boost 1.55 compatible with C++11
 wget --quiet -O boost_1_55_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download
 tar -xzf boost_1_55_0.tar.gz
@@ -23,13 +31,6 @@ sudo ldconfig
 # sudo apt-get update -qq
 # if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
 # if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
-
-# Install gcc 4.8 for C++11
-sudo apt-get --yes install python-software-properties 
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get --yes install gcc-4.8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 
 sudo add-apt-repository --yes ppa:libccd-debs/ppa
 sudo add-apt-repository --yes ppa:fcl-debs/ppa
