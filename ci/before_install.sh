7a612c989e8044e494d27ce322e3046ebfa87c0c
Jeongseok Lee <jslee02@gmail.com>
2015-01-14 18:58:51 -0500

    Testing gcc 4.8 install

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -15,53 +15,55 @@ sudo apt-get --yes install gcc-4.8
 sudo apt-get --yes autoremove
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 
+gcc --version
+
 # Install boost 1.55 compatible with C++11
-wget --quiet -O boost_1_55_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download
-tar -xzf boost_1_55_0.tar.gz
-cd boost_1_55_0/
-sudo apt-get update -qq
-sudo apt-get install -qq build-essential g++ python-dev autotools-dev libicu-dev build-essential libbz2-dev 
-./bootstrap.sh --prefix=/usr/local
-n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
-sudo ./b2 --with=all -j $n install 
-sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/local.conf'
-sudo ldconfig
+# wget --quiet -O boost_1_55_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download
+# tar -xzf boost_1_55_0.tar.gz
+# cd boost_1_55_0/
+# sudo apt-get update -qq
+# sudo apt-get install -qq build-essential g++ python-dev autotools-dev libicu-dev build-essential libbz2-dev 
+# ./bootstrap.sh --prefix=/usr/local
+# n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
+# sudo ./b2 --with=all -j $n install 
+# sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/local.conf'
+# sudo ldconfig
 
 # sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 # sudo apt-get update -qq
 # if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
 # if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
 
-sudo add-apt-repository --yes ppa:libccd-debs/ppa
-sudo add-apt-repository --yes ppa:fcl-debs/ppa
-sudo add-apt-repository --yes ppa:dartsim/ppa
-sudo apt-get update
+# sudo add-apt-repository --yes ppa:libccd-debs/ppa
+# sudo add-apt-repository --yes ppa:fcl-debs/ppa
+# sudo add-apt-repository --yes ppa:dartsim/ppa
+# sudo apt-get update
 
-APT_CORE='
-cmake
-freeglut3-dev
-libassimp-dev
-libccd-dev
-libeigen3-dev
-libfcl-dev
-libxi-dev
-libxmu-dev
-'
+# APT_CORE='
+# cmake
+# freeglut3-dev
+# libassimp-dev
+# libccd-dev
+# libeigen3-dev
+# libfcl-dev
+# libxi-dev
+# libxmu-dev
+# '
 
-APT=$APT_CORE' 
-libflann-dev
-libgtest-dev
-libtinyxml-dev
-libtinyxml2-dev
-liburdfdom-dev
-liburdfdom-headers-dev
-'
+# APT=$APT_CORE' 
+# libflann-dev
+# libgtest-dev
+# libtinyxml-dev
+# libtinyxml2-dev
+# liburdfdom-dev
+# liburdfdom-headers-dev
+# '
 
-if [ $BUILD_CORE_ONLY = OFF ]; then
-  sudo apt-get --yes --force-yes install $APT
-else
-  sudo apt-get --yes --force-yes install $APT_CORE
-fi
+# if [ $BUILD_CORE_ONLY = OFF ]; then
+#   sudo apt-get --yes --force-yes install $APT
+# else
+#   sudo apt-get --yes --force-yes install $APT_CORE
+# fi
 
 # (before_install)
 
